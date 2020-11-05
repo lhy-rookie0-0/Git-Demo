ad00d5ec0fd25476d395c0b8d3da9419dc164f19
${lhy-rookie0-0} <${1789125262@qq.com}>
2020-11-05 21:06:36 +0800

    master的第2次修改(冲突的模拟)

--- MyFirstGitProject/src/com/lhy/GitDemo.java
+++ MyFirstGitProject/src/com/lhy/GitDemo.java
@@ -3,5 +3,8 @@ package com.lhy;
 public class GitDemo {
     public static void main(String[] args){
         System.out.println("Hello Git");
+
+        System.out.println("Hello master");
+
     }
 }
